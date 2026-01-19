c71b9a1ada1f197d9e1e5290981fd4a898d44fd9
bour2012 <jarawee2310@gmail.com>
2026-01-19 19:29:42 +0700

    Fix Main Menu
    - ปรับ main menu

--- Assets/Scripts/MenuParallax.cs
+++ Assets/Scripts/MenuParallax.cs
@@ -7,17 +7,35 @@ public class MenuParallax : MonoBehaviour
 
     private Vector2 startPosition;
     private Vector3 velocity;
-
     void Start()
     {
-        startPosition = transform.position;
+        // เปลี่ยนเป็น localPosition เพื่อจำตำแหน่งเทียบกับ Parent
+        startPosition = transform.localPosition;
     }
 
-
-    // Update is called once per frame
     void Update()
     {
         Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+
+        // คำนวณ offset โดยให้ (0.5, 0.5) คือจุดกึ่งกลาง (เมาส์อยู่กลางจอ = ไม่ขยับ)
+        Vector2 centeredOffset = offset - new Vector2(0.5f, 0.5f);
+
+        // เป้าหมายคือ ตำแหน่ง local เดิม + การขยับตามเมาส์
+        Vector3 targetPosition = startPosition + (centeredOffset * offsetMultiplier);
+
+        // ใช้ localPosition ในการขยับ
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPosition, ref velocity, smoothTime);
     }
+    //void Start()
+    //{
+    //    startPosition = transform.position;
+    //}
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+    //    transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+    //}
 }
